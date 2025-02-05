0fff6c130cf8c21c5140999d8abe9f6888543e72
Pedro Eyer <pedrojulioeyr@gmail.com>
2025-02-05 20:34:29 -0300

    finished

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 let btnMenu = document.getElementById('btn-menu');
 let menu = document.getElementById('menu-mobile');
 let overlay = document.getElementById('overlay-menu');
+let btnFechar = document.getElementById('btn-fechar');
 
 btnMenu.addEventListener('click', () => {
     menu.classList.add('abrir-menu');
+    overlay.style.display = 'block';
 });
-menu.addEventListener('click', () => {
+
+btnFechar.addEventListener('click', () => {
     menu.classList.remove('abrir-menu');
+    overlay.style.display = 'none';
 });
+
 overlay.addEventListener('click', () => {
     menu.classList.remove('abrir-menu');
+    overlay.style.display = 'none';
+});
+
+// Fechar o menu ao clicar em um link
+const menuLinks = menu.querySelectorAll('a');
+menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        menu.classList.remove('abrir-menu');
+        overlay.style.display = 'none';
+    });
 });
